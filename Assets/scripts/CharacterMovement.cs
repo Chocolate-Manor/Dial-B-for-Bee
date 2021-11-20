aaeb894a1410714312b077406f8c9a89b2c97f6a
Jorge Romeu Huidobro <jorge.romeu.huidobro@gmail.com>
2021-11-20 16:56:03 +0100

    Implement movement

--- Assets/scripts/CharacterMovement.cs
+++ Assets/scripts/CharacterMovement.cs
@@ -6,27 +6,35 @@ public class CharacterMovement : MonoBehaviour
 {
 
     public float speed = 10;
-    
+  
+    [SerializeField]
     private Rigidbody2D rb;	
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        // rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mouse = Input.mousePosition;
-        Vector3 mouseGameCoords = Camera.main.ScreenToWorldPoint(mouse);
+        var mouse = Input.mousePosition;
+        var mouseGameCoords = Camera.main.ScreenToWorldPoint(mouse);
 
         // update player orientation
-        transform.LookAt(new Vector3(0.0f, 0.0f, 1.0f), mouseGameCoords);
+        // transform.LookAt(new Vector3(0.0f, 0.0f, 1.0f), mouseGameCoords);
 
-        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        Vector2 moveVelocity = moveInput * speed;
-        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        var dir = new Vector2(mouseGameCoords.x - transform.position.x, mouseGameCoords.y - transform.position.y);
+        transform.up = dir;
+
+        // rb.MoveRotation();
+        // rb.rotation = mouse.x;
+        // Debug.Log(rb.rotation);
+
+        // var moveVelocity = moveInput * speed;
+        var moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb.velocity = moveInput * speed;
 
     }
 }
