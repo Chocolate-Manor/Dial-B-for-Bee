9edc3b39c8863f151f26df65759d2e6676a82503
Pavlos Makridis <p.makridis@student.tudelft.nl>
2021-11-21 23:12:51 +0100

    Add meta turret files

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    public float speed = 10;
+  
+    [SerializeField]
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get mouse coords
+        var mouse = Input.mousePosition;
+        var mouseGameCoords = Camera.main.ScreenToWorldPoint(mouse);
+
+        // orient the transform towards mouse
+        var dir = new Vector2(mouseGameCoords.x - transform.position.x, mouseGameCoords.y - transform.position.y);
+        transform.up = dir;
+    }
+
+    private void FixedUpdate()
+    {
+        // var moveVelocity = moveInput * speed;
+        var moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb.velocity = moveInput * speed;
+    }
+}
---
+++ Assets/Scripts/Ladybug.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladybug : MonoBehaviour
+{
+    [SerializeField] private float lifeSpan = 500;
+    // Destroy(gameObject, lifeSpan);
+
+    private float initializationTime;
+
+    [SerializeField] private Rigidbody2D rb;
+
+    [SerializeField] private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initializationTime = Time.timeSinceLevelLoad;
+        rb.AddForce(transform.up * speed);
+        Destroy(gameObject, lifeSpan);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
