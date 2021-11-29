6c08b2cbb3f708c20cd789eae470a8e1ac24d93e
Haocheng <H.Yang-15@student.tudelft.nl>
2021-11-29 15:57:07 +0100

    add particles for box breaking

--- Assets/Scripts/Entities/Crate.cs
+++ Assets/Scripts/Entities/Crate.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Crate : MonoBehaviour, IDamagable
 {
     [SerializeField] private AudioClip crateBreak;
-
+    [SerializeField] private GameObject boxBreakParticle;
+    
     public void Damage()
     {
         GameManager.instance.PlaySoundEffect(crateBreak);
+        Instantiate(boxBreakParticle, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
