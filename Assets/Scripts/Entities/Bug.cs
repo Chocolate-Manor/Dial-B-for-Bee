1b81bbb8ee65f6468a774e17db72183ad56b13ec
Pavlos Makridis <p.makridis@student.tudelft.nl>
2021-11-29 16:55:28 +0100

    Add pickup sound

--- Assets/Scripts/Entities/Bug.cs
+++ Assets/Scripts/Entities/Bug.cs
@@ -4,13 +4,15 @@ public class Bug : MonoBehaviour
 {
     public string name;
     public bool isPickable = true;
-
+    public AudioClip pickupSound;
+    
     public void PickMeUp(B player)
     {
         if (isPickable)
         {
             int index = player.bugNames.FindIndex(x => x.Equals(name));
             player.bugCounts[index]++;
+            GameManager.instance.PlaySoundEffect(pickupSound);
         }
     }
 }
