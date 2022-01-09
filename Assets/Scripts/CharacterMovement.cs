4c098502e462171611516f9838ec843243e2f4a4
howchen217 <59067683+howchen217@users.noreply.github.com>
2022-01-09 19:14:06 +0100

    add walking B

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,7 +5,9 @@ public class CharacterMovement : MonoBehaviour
     public float speed = 10;
 
     [SerializeField] private Rigidbody2D rb;
-
+    [SerializeField] private Animator animator;
+    [SerializeField] private AudioClip footstepSound;
+    
     // Update is called once per frame
     void Update()
     {
@@ -19,6 +21,15 @@ public class CharacterMovement : MonoBehaviour
             var dir = new Vector2(mouseGameCoords.x - transform.position.x, mouseGameCoords.y - transform.position.y);
             transform.up = dir;
         }
+
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
     }
 
     private void FixedUpdate()
@@ -26,5 +37,13 @@ public class CharacterMovement : MonoBehaviour
         // var moveVelocity = moveInput * speed;
         var moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         rb.velocity = moveInput * speed;
+    }
+    
+    /// <summary>
+    /// Associated with animation event of B walking. 
+    /// </summary>
+    public void playFootstepSound()
+    {
+        GameManager.Instance.PlaySoundEffect(footstepSound);
     }
 }
