8f2b4971cfd65c1e4dd7eeceebc0cf915088676d
howchen217 <59067683+howchen217@users.noreply.github.com>
2022-01-11 16:58:19 +0100

    make walking work without sideway

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -7,10 +7,12 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Animator animator;
     [SerializeField] private AudioClip footstepSound;
+    [SerializeField] private AudioSource audioSource;
     
     // Update is called once per frame
     void Update()
     {
+        
         if (!PauseMenu.IsPaused)
         {
             // get mouse coords
@@ -18,20 +20,53 @@ public class CharacterMovement : MonoBehaviour
             var mouseGameCoords = Camera.main.ScreenToWorldPoint(mouse);
 
             // orient the transform towards mouse
-            var dir = new Vector2(mouseGameCoords.x - transform.position.x, mouseGameCoords.y - transform.position.y);
+            Vector2 dir = new Vector2(mouseGameCoords.x - transform.position.x, mouseGameCoords.y - transform.position.y);
             transform.up = dir;
+            
+            
+            // if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            // {
+            //     animator.SetBool("isWalking", true);
+            // }
+            // else
+            // {
+            //     animator.SetBool("isWalking", false);
+            // }
+        
+            animator.SetFloat("Blend",  Input.GetAxis("Horizontal") + Input.GetAxis("Vertical"));
+        
+
+            //get the velocity in mouse direction, normalized
+            float velocityInDirection = Vector3.Dot(rb.velocity.normalized, dir.normalized);
+            //Debug.Log(velocityInDirection);
+            
+            animator.SetFloat("Blend",  velocityInDirection);
+
+            bool hasInput = Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0; 
+            if (Mathf.Abs(velocityInDirection) > 0.7 && hasInput)
+            {
+                animator.SetBool("isWalking", true);
+            }
+            else
+            {
+                animator.SetBool("isWalking", false);
+            }
+
+            if (hasInput)
+            {
+                audioSource.UnPause();
+            }
+            else
+            {
+                audioSource.Pause();
+            }
+            
+            
         }
 
-        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
-        {
-            animator.SetBool("isWalking", true);
-        }
-        else
-        {
-            animator.SetBool("isWalking", false);
-        }
+;
         
-        animator.SetFloat("Blend", Input.GetAxis("Horizontal") + Input.GetAxis("Vertical"));
+
     }
 
     private void FixedUpdate()
