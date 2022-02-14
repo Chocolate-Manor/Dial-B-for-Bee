24fb8fe3e4da20871d86d977e173e11f9553fba2
howchen217 <59067683+howchen217@users.noreply.github.com>
2022-02-14 21:07:53 +0100

    make spider web

--- Assets/Scripts/Entities/Bugs/SpiderWeb.cs
+++ Assets/Scripts/Entities/Bugs/SpiderWeb.cs
@@ -1,38 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpiderWeb : MonoBehaviour
 {
-    public Rigidbody2D hook;
-
-    public GameObject[] prefabRopeSegs;
-
-    public int numLinks = 3;
-
+    [SerializeField] private Rigidbody2D firstSeg;
     void Start()
     {
-        GenerateRope();
-        
-        //need to provide a force forward that it shoots forward into an object, however..
+        //firstSeg.velocity = transform.up * 50;
     }
 
-    void GenerateRope()
+    private void Update()
     {
-        Rigidbody2D prevRb = hook;
-        for (int i = 0; i < numLinks; i++)
+        if (Input.GetKeyDown(KeyCode.I))
         {
-            int segIndex = Random.Range(0, prefabRopeSegs.Length);
-            GameObject newSeg = Instantiate(prefabRopeSegs[segIndex]);
-
-            //set rope as segments' parent, as well as position
-            newSeg.transform.parent = transform;
-            newSeg.transform.position = transform.position;
-            
-            //set connectedBody as prevRb
-            newSeg.GetComponent<HingeJoint2D>().connectedBody = prevRb;
-
-            prevRb = newSeg.GetComponent<Rigidbody2D>();
-        }
+            firstSeg.velocity = transform.up * 50; 
+        } 
     }
 }
